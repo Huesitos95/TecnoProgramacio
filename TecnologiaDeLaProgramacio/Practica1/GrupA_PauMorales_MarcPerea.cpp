d5ac267e62e3ea1b1e898b75ecc15252e0f0f9ea
Pau Morales <pau.morales.torrent@gmail.com>
2017-09-27 12:44:09 +0200

    Practica 1 init

---
+++ TecnologiaDeLaProgramacio/Practica1/GrupA_PauMorales_MarcPerea.cpp
@@ -0,0 +1,14 @@
+// Contenidors STL
+// Vector, deque, queue, priority_queue, stack, list, forward_list, map i set
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <forward_list>
+#include <map>
+#include <set>
+
+
+
